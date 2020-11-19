ffadcdbbbd7283eff5458555ade597d13070804d
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-19 12:00:45 -0600

    on 3

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace linq
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L")).ToList();
+            Console.WriteLine("The L Fruits");
+            Console.WriteLine(LFruits);
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 2 == 0);
+            Console.WriteLine(fourSixMultiples);
+
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+            List<string> descend = names.OrderByDescending(n => n);
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers = new List<int>()
+                {
+                    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+                };
+
+        }
+    }
+}
