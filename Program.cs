eefa215d997b2526c1731f5949ed2378440dab60
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-19 13:31:22 -0600

    most expensive

--- Program.cs
+++ Program.cs
@@ -67,6 +67,16 @@ namespace linq
             double totalAmount = purchases.Sum();
             Console.WriteLine("Total money made");
             Console.WriteLine(totalAmount);
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+                {
+                    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+                };
+            double expensive = prices.Max();
+            Console.WriteLine("Most expensive");
+            Console.WriteLine(expensive);
+
         }
     }
 }
